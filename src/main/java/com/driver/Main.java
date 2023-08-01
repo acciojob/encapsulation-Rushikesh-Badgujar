c7e8329b3be14a42a47f97d94a094acc640a3574
Rushikesh-Badgujar <rushikeshbadgujar1000@gmail.com>
2023-08-01 13:57:09 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+         RWOnly obj = new RWOnly();
+         obj.name = "John";
+         obj.setName("John");
+         System.out.println("Name:"+obj.getName());
+    }
 }
